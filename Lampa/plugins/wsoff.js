8661a4a548eb831d4544a04950c3cc318d3d7b1b
war100ck <war100ck2008@yandex.ru>
2023-11-15 11:53:44 +0200

    𝖀𝖕𝖉𝖆𝖙𝖊 𝖕𝖑𝖚𝖌𝖎𝖓𝖘

---
+++ Lampa/plugins/wsoff.js
@@ -0,0 +1,3 @@
+;
+Lampa.Socket.init = function(){console.log('Socket', 'initialization disabled by wsoff.js plugin')};
+Lampa.Socket.send = function(method, data){console.log('Socket', 'Method "' + method + '" send disabled by wsoff.js plugin')};
